956df3f1938d166f31303b4722809c6c0bd700ae
L1ghtzin <lightzin021@gmail.com>
2024-02-23 10:48:22 -0300

    ak3: Rebrand to channel
    Change-Id: I786c814be24bdbbc0e54114a37ec8277f2a6bfb4

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,16 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Skyline
-kernel.compiler=AOSP clang
-kernel.made=SHAND
+kernel.string=Supra
+kernel.made=L1ghtzin_
 message.word=
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=aljeter
-device.name2=jeter
-supported.versions=11.0-13.0
+device.name1=channel
+supported.versions=11.0-14.0
 supported.patchlevels=
 '; } # end properties
 
@@ -27,10 +25,9 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 
-## boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
+block=/dev/block/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=lzma;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
